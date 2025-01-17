4596e6d98ae345dbcb5eaf886f5e0a579a08dfdd
Jeremy Plichta <jeremy.plichta@redis.com>
2025-01-16 17:39:13 -0700

    refactor into re_api function

--- grafana/demo/setup.sh
+++ grafana/demo/setup.sh
@@ -7,30 +7,60 @@ then
   exit 1
 fi
 
+function re_api () {
+    BASE_URL="https://localhost:9443"
+    ACTION=$1
+    PATHNAME=$2
+    DATA=$3
+    RETRIES=12
+    if [ -n "$DATA" ]
+    then
+      EXTRA_ARGS="-HContent-Type:application/json -d '$DATA'"
+    else
+      EXTRA_ARGS=""
+    fi
+    if [[ $ACTION != "create cluster" ]]
+    then
+      EXTRA_ARGS="-u demo@redis.com:redislabs $EXTRA_ARGS"
+    fi
+
+    N=0
+    echo $ACTION ...
+    echo > ./lastapioutput
+    echo > ./lastcode
+    while [[ $N -lt $RETRIES && "$(eval curl -o ./lastapioutput -w '%{http_code}' $EXTRA_ARGS -k https://localhost:9443${PATHNAME} 2>/dev/null | tee ./lastcode)" != "200" ]];
+    do
+       N=$((N+1))
+       echo "still trying to ${ACTION} $(cat ./lastcode) $(cat ./lastapioutput)"
+       sleep 5;
+    done
+
+    if [ $N -ge $RETRIES ]
+    then
+        echo "failed to $ACTION with max retries"
+        exit 1
+    fi
+    echo ""
+}
+
 docker compose up -d
 
 container_name="demo-redis-1"
 
-echo "waiting for the servers..."
-sleep 60
-echo "creating cluster..."
 
-while [[ "$(curl -o /dev/null  -w ''%{http_code}'' -X POST -H 'Content-Type:application/json' -d '{"action":"create_cluster","cluster":{"name":"dashboard.local"},"node":{"paths":{"persistent_path":"/var/opt/redislabs/persist","ephemeral_path":"/var/opt/redislabs/tmp"}},"credentials":{"username":"demo@redis.com","password":"redislabs"}}' -k https://localhost:9443/v1/bootstrap/create_cluster)" != "200" ]]; do sleep 5; done
-
-# Test the cluster. cluster info and nodes
-while [[ "$(curl -o ./bootstrap -w ''%{http_code}'' -u demo@redis.com:redislabs -k https://localhost:9443/v1/bootstrap)" != "200" ]]; do sleep 5; done
-while [[ "$(curl -o ./nodes -w ''%{http_code}'' -u demo@redis.com:redislabs -k https://localhost:9443/v1/nodes)" != "200" ]]; do sleep 5; done
-echo ""
-echo "bootstrap..." && cat ./bootstrap
-echo ""
-echo "nodes..." && cat ./nodes
-echo ""
+re_api "create cluster" "/v1/bootstrap/create_cluster" '{"action":"create_cluster","cluster":{"name":"dashboard.local"},"node":{"paths":{"persistent_path":"/var/opt/redislabs/persist","ephemeral_path":"/var/opt/redislabs/tmp"}},"credentials":{"username":"demo@redis.com","password":"redislabs"}}'
+re_api "bootstrap cluster" /v1/bootstrap
+re_api "get nodes" /v1/nodes
+re_api "get modules" /v1/modules
+while [[ "$(cat ./lastapioutput)" == "[]" ]]
+do
+  echo "modules not yet loaded, will try again soon"
+  sleep 10
+  re_api "get modules" /v1/modules
+done
 
 # Get the module info to be used for database creation
-while [[ "$(curl -o ./modules.txt -w ''%{http_code}'' -u demo@redis.com:redislabs -k https://localhost:9443/v1/modules)" != "200" ]]; do sleep 5; done
-echo ""
-echo "modules..." && cat ./modules.txt
-
+cp ./lastapioutput ./modules.txt
 # modules endpoint returns multiple versions supported, use latest (first), filter it with jq
 json_module_name=$(cat ./modules.txt | jq '[.[] | select(.module_name == "ReJSON")] | first | .module_name' -r)
 json_semantic_version=$(cat ./modules.txt | jq '[.[] | select(.module_name == "ReJSON")] | first | .semantic_version' -r)
@@ -39,19 +69,11 @@ search_semantic_version=$(cat ./modules.txt | jq '[.[] | select(.module_name == 
 timeseries_module_name=$(cat ./modules.txt |jq '[.[] | select(.module_name == "timeseries")] | first | .module_name' -r)
 timeseries_semantic_version=$(cat ./modules.txt | jq '[.[] | select(.module_name == "timeseries")] | first | .semantic_version' -r)
 
-while [[ "$(curl -o ./dashboard -w ''%{http_code}'' -u demo@redis.com:redislabs -X POST -H "Content-Type: application/json" -d "{\"email\": \"dashboard@redis.com\",\"password\": \"dashboard\",\"name\": \"dashboard\",\"email_alerts\": false,\"role\": \"db_member\"}" -k https://localhost:9443/v1/users 2>/dev/null | tee dashboard.out)" != "200" ]]; do echo "still trying to create dashboard user $(cat dashboard.out)"; cat dashboard; sleep 5; done
-echo "user dashboard..." && cat ./dashboard
-echo ""
+re_api "create role" "/v1/roles" '{"name": "db_member", "management": "db_member"}'
+re_api "create user" /v1/users "{\"email\": \"dashboard@redis.com\",\"password\": \"dashboard\",\"name\": \"dashboard\",\"email_alerts\": false,\"role\": \"db_member\"}"
 
-echo ""
-echo "creating databases..."
 echo creating dashboard-demo database with "${search_module_name}" version "${search_semantic_version}" and "${json_module_name}" version "${json_semantic_version}"
-
-while [[ "$(curl -o ./dashboard-demo -w ''%{http_code}'' -u demo@redis.com:redislabs --location-trusted -H "Content-type:application/json" -d '{ "name": "dashboard-demo", "port": 12000, "memory_size": 500000000, "type" : "redis", "replication": false, "default_user": false, "roles_permissions": [{"role_uid": 2, "redis_acl_uid": 1}], "module_list": [ {"module_args": "PARTITIONS AUTO", "module_name": "'"$search_module_name"'", "semantic_version": "'"$search_semantic_version"'"}, {"module_args": "", "module_name": "'"$json_module_name"'", "semantic_version": "'"$json_semantic_version"'"} ] }' -k https://localhost:9443/v1/bdbs 2>/dev/null | tee database.out)" != "200" ]]; do echo "still trying to create database $(cat database.out)"; cat ./dashboard-demo ; sleep 5; done
-
-echo ""
-echo "database dashboard-demo..." && cat ./dashboard-demo
-echo ""
+re_api "create databases" /v1/bdbs "{\"name\": \"dashboard-demo\", \"port\": 12000, \"memory_size\": 500000000, \"type\" : \"redis\", \"replication\": false, \"default_user\": false, \"roles_permissions\": [{\"role_uid\": 2, \"redis_acl_uid\": 1}], \"module_list\": [ {\"module_args\": \"PARTITIONS AUTO\", \"module_name\": \"$search_module_name\", \"semantic_version\": \"$search_semantic_version\"}, {\"module_args\": \"\", \"module_name\": \"$json_module_name\", \"semantic_version\": \"$json_semantic_version\"} ] }"
 
 # Enable bdb name
 docker exec -it "${container_name}" bash -c "/opt/redislabs/bin/ccs-cli hset cluster_settings metrics_exporter_expose_bdb_name enabled"
@@ -64,8 +86,7 @@ echo "You can open a browser and access Redis Enterprise Admin UI at https://127
 echo "DISCLAIMER: This is best for local development or functional testing. Please see, https://docs.redis.com/latest/rs/installing-upgrading/quickstarts/docker-quickstart/"
 
 # Cleanup
-rm bootstrap nodes modules.txt dashboard-demo
-#rm list_modules.sh create_cluster.* module_list.txt
+rm ./lastapioutput ./lastcode 2>/dev/null
 docker exec --user root "${container_name}" bash -c "rm /opt/list_modules.sh"
 docker exec --user root "${container_name}" bash -c "rm /opt/module_list.txt"
 docker exec --user root "${container_name}" bash -c "rm /opt/create_cluster.*"

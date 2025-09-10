2ba5dd94c6159687e345faffc437ad463330d8c2
slorello89 <steve.lorello@redis.com>
2025-09-10 15:43:08 -0400

    updates to v2 alerts

--- grafana_v2/kickstart_v2/terraform/gcp/main.tf
+++ grafana_v2/kickstart_v2/terraform/gcp/main.tf
@@ -205,7 +205,7 @@ resource "null_resource" "run-kickstart" {
     provisioner "remote-exec" {
             inline = [
                 "cd redis-enterprise-observability/grafana_v2/kickstart_v2",
-                "git checkout main",
+                "git checkout ${var.git_branch}",
                 "./setup.sh ${local.redis_db_primary_fqdn} ../dashboards/grafana_v9-11/cloud/basic",
             ]
         }
